97bc0082ad91f2171aed074d5159b5a3c1b63670
sebranly <sebranly.pinf@gmail.com>
2022-12-12 17:18:01 +0100

    feat: push initial commit based off template-ts repository

---
+++ src/constants/general.ts
@@ -0,0 +1,6 @@
+export const AUTHOR_NAME = 'sebranly';
+export const PROJECT_NAME = 'kaleidoscope';
+export const PROJECT_URL = `https://github.com/${AUTHOR_NAME}/${PROJECT_NAME}`;
+export const CHANGELOG_URL = `${PROJECT_URL}/releases`;
+export const WEBSITE_TITLE = 'Kaleidoscope';
+export const WEBSITE_VERSION = '0.0.1';
