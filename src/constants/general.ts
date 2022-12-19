5cb5e662f231c66eebea400cd72a744d47ad408c
Sebastien Branly <sebranly.pinf@gmail.com>
2022-12-19 19:55:54 +0100

    First public version (#5)

--- src/constants/general.ts
+++ src/constants/general.ts
@@ -4,7 +4,7 @@ export const PROJECT_URL = `https://github.com/${AUTHOR_NAME}/${PROJECT_NAME}`;
 export const CHANGELOG_URL = `${PROJECT_URL}/releases`;
 export const WEBSITE_TITLE = 'Kaleidoscope';
 export const WEBSITE_URL = `https://${AUTHOR_NAME}.github.io/${PROJECT_NAME}`;
-export const WEBSITE_VERSION = '0.0.1';
+export const WEBSITE_VERSION = '1.0.0';
 export const EPISODE_COUNT = 8;
 export const TIME_API_URL = 'https://worldtimeapi.org/api/timezone/America/Los_Angeles';
 // Result of `Math.floor((new Date("2023-01-01T00:00:00.000-08:00")).getTime() / 1000)`
